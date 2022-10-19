e916c97e4070618a1ac0c760b564aed14b596e46
Khalid Mohammed <khalarasta104@gmail.com>
2022-10-19 14:48:03 +0300

    Hackerrank-Insertion Sort Part 1 solution

---
+++ Insertion-Sort-Part-1-Hackerrank.js
@@ -0,0 +1,58 @@
+/** @format */
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'insertionSort1' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+
+function insertionSort1(n, arr) {
+  // Write your code here
+  if (n > 1) {
+    let temp = arr[n - 1];
+    for (let i = n - 2; i >= -1; i--) {
+      if (i == -1 || temp > arr[i]) {
+        arr[i + 1] = temp;
+        break;
+      }
+      arr[i + 1] = arr[i];
+      console.log(arr.join(" "));
+    }
+  }
+  console.log(arr.join(" "));
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  insertionSort1(n, arr);
+}
