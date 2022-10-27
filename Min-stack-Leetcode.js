a37dc11bd314bf40c350e3b7ed8e60cd1c479565
Khalid Mohammed <khalarasta104@gmail.com>
2022-10-27 20:09:39 +0300

    Leetcode-Min stack solution

---
+++ Min-stack-Leetcode.js
@@ -0,0 +1,45 @@
+/** @format */
+
+var MinStack = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  const minVal =
+    this.stack.length === 0 ? val : this.stack[this.stack.length - 1].minVal;
+  this.stack.push({ val, minVal: Math.min(val, minVal) });
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1].val;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.stack[this.stack.length - 1].minVal;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
